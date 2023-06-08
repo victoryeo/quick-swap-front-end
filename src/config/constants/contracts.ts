c294548028103f93403a700143e5fddf5d545c3d
yeosiowvic <victoryeo1974@gmail.com>
2023-06-08 20:11:51 +0800

    add chiado chain

--- src/config/constants/contracts.ts
+++ src/config/constants/contracts.ts
@@ -2,6 +2,7 @@ const contracts = {
   GRIEFING_LOCK: {
     5: '0x224266B5e1033d8e6449D7bB0e86f84e8F15ac5D',
     100: '0xaEF1CA2d215AF72F4AA574629e0629aAaDe1349c',
+    10200: '',
   },
   GRIEFING_LOCK_TTOKEN: {
     5: '',
@@ -9,6 +10,7 @@ const contracts = {
   PRINCIPAL_LOCK: {
     5: '0xCAEABa7ED01AEC7d141F2D8C38b8381EBd2e770e',
     100: '0x1174C0C205bCD2F330ff60b7eCd0C7D7Db16Eb82',
+    10200: '',
   },
   PRINCIPAL_LOCK_TTOKEN: {
     5: '',
