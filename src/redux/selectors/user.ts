ed0d9a774f3d8ba75d4e944c47d44014a253a5b6
yeosiowvic <victoryeo1974@gmail.com>
2023-06-08 10:16:20 +0800

    add principal lock select

--- src/redux/selectors/user.ts
+++ src/redux/selectors/user.ts
@@ -1,3 +1,7 @@
 import type { AppState } from '../../store/store'
 
 export const selectUserAddress = (state: AppState) => state.user.userAddress;
+
+export const selectGoerliPlock = (state: AppState) => state.user.goerliPlock;
+
+export const selectGnosisPlock = (state: AppState) => state.user.gnosisPlock;
